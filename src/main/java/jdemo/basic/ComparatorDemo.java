f11335d761babb001dd4fc92c645833df1f5d3e2
陈濛 <chenmeng.yzy@bytedance.com>
2022-06-10 16:46:18 +0800

    update

--- src/main/java/jdemo/basic/ComparatorDemo.java
+++ src/main/java/jdemo/basic/ComparatorDemo.java
@@ -3,7 +3,6 @@ package jdemo.basic;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.ToIntFunction;
 
 public class ComparatorDemo {
     public static void main(String[] args) {
